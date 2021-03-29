82d31a3afffe5fb825953c39e5bbabcef1dae3ba
msafwankarim <66724151+msafwankarim@users.noreply.github.com>
2021-03-29 14:47:04 +0500

    Add files via upload

---
+++ protector.py
@@ -0,0 +1,52 @@
+import sqlite3
+import argparse
+import os
+
+db_conn = ""
+def init():
+    global db_conn
+    user = os.environ.get('USERNAME')
+    if user != 'Safwan':
+        print("Unauthorized use of code\nexiting...")
+    db_conn = sqlite3.connect("C:\\Users\\Safwan\\temp_logs.db")
+    if db_conn is None:
+        raise ConnectionError("Error in finding file")
+
+
+def find(domain):
+    query = "SELECT * FROM temporary WHERE domain='"+domain+"';"
+    # print(query)
+    cursor = db_conn.execute(query)
+    for row in cursor:
+        print(f"{row[2]}")
+
+
+def main():
+    init()
+    parser = argparse.ArgumentParser(description="Argument Switch parser")
+    parser.add_argument("-d", "--domain", metavar='domain',
+                        type=str, help='title for the phrase')
+    parser.add_argument("-a", "--all", action='store_true',
+                        help='Get list of all domains')
+    # parser.add_argument("-p", "--phrase", action="store",
+                        # help="Phrase to store")
+    # parser.add_argument('-p','--phrase',metavar='phrase',type=str, help='pharase to store')
+
+    listall = parser.parse_args().all
+    domain = parser.parse_args().domain
+    # phrase = parser.parse_args().phrase
+
+    # print(parser.parse_args())
+    if domain is None and not listall:
+        parser.print_help()
+    elif listall:
+        cursor = db_conn.execute("SELECT domain FROM temporary")
+        print("Domain\n=======")
+        for row in cursor:
+            print(row[0])
+
+    elif domain:
+        find(domain)
+
+
+main()
